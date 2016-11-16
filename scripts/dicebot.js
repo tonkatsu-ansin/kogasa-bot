1909e6d029ba07df18ee3b5bab764009391b189e
himanoa <matsunoappy@gmail.com>
2016-11-16 16:32:46 +0900

    heroku

--- scripts/dicebot.js
+++ scripts/dicebot.js
@@ -1,15 +1,17 @@
 var xorshift = require('xorshift');
-function* range (begin, end, interval = 1) {
-  for (let i = begin; i < end; i += interval) {
-    yield i;
-  }
+ function* range (begin, end, interval = 1) {
+   for (let i = begin; i < end; i += interval) {
+     yield i;
+   }
 }
 module.exports = function(robot){
   robot.hear(/([1-9][0-9]*)d([1-9][0-9]*)/, function(msg){
     const trials = parseInt(msg.match[1], 10);
     const diceCount = parseInt(msg.match[2], 10);
-    const result =
-      [Math.floor(xorshift.random() * 6) + 1 for x in range(0, trials)];
-    msg.reply(`${msg.match[0]} -> ${result} = ${result.reduce((prev, current) => prev + current;)}`)
-  });
+    var results = [];
+    for(let i of range(0, trials)){
+      results.push(Math.floor(xorshift.random() * 6) +1);
+    }
+    msg.reply(`${msg.match[0]} -> ${results} = ${results.reduce((prev, current) => prev + current)}`);
+  })
 }
