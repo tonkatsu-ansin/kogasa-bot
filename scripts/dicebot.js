bd9434d0a9ba6aab665c573516e6593f14d905bf
himanoa <matsunoappy@gmail.com>
2016-11-16 17:00:17 +0900

    a

--- scripts/dicebot.js
+++ scripts/dicebot.js
@@ -6,12 +6,16 @@ var xorshift = require('xorshift');
 }
 module.exports = function(robot){
   robot.hear(/([1-9][0-9]*)d([1-9][0-9]*)/, function(msg){
-    const trials = parseInt(msg.match[1], 10);
-    const diceCount = parseInt(msg.match[2], 10);
-    var results = [];
-    for(let i of range(0, trials)){
-      results.push(Math.floor(xorshift.random() * diceCount) +1);
+    try{
+      const trials = parseInt(msg.match[1], 10);
+      const diceCount = parseInt(msg.match[2], 10);
+      var results = [];
+      for(let i of range(0, trials)){
+        results.push(Math.floor(xorshift.random() * diceCount) +1);
+      }
+      msg.reply(`${msg.match[0]} -> ${results} = ${results.reduce((prev, current) => prev + current)}`);
+    }catch(e){
+      msg.reply('おどろけーっ');
     }
-    msg.reply(`${msg.match[0]} -> ${results} = ${results.reduce((prev, current) => prev + current)}`);
   })
 }
